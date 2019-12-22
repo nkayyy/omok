6f9e3feac10a7d6032aa9454ece88613f9870f64
MSkim <51520671+swjot@users.noreply.github.com>
2019-12-21 23:27:25 -0500

    Create piece.cc

---
+++ piece.cc
@@ -0,0 +1,16 @@
+#include <string>
+#include "piece.h"
+
+using namespace std;
+   
+Piece(string type, int row, int col){
+    this -> type;
+    this -> row;
+    this -> col;
+}
+int getRow(){
+    return row;
+}
+int getCol(){
+    retrun col;
+}
